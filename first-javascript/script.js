9ba88136162e244f23b49b0c7209685913cff909
david.tarvin <david.tarvin@physiciansmutual.com>
2023-06-15 14:56:27 -0500

    changes

---
+++ first-javascript/script.js
@@ -0,0 +1,355 @@
+// var student = "Jill";
+// console.log(student);
+// console.log("The name of the student is " + student);
+// console.log("student");
+// console.log(2);
+// console.log('2');
+// console.log("good" + "morning");
+// console.log("good" + " " + "morning");
+// console.log("good " + "morning");
+// console.log("good" + " morning");
+// var firstName = "Evan";
+// var lastName = "Henderson";
+// console.log(firstName + " " + lastName);
+// console.log(2 + 5);
+// console.log('2' + '5');
+// console.log(2 + '5');
+// console.log('2' + 5);
+// var cities = ["Oakland", "Chicago", "Pittsburgh"];
+// console.log(cities[1]);
+// console.log(cities[2]);
+// console.log(cities[0]);
+// console.log(cities[3]);
+// var points = [25, 36, 42, 81, 50];
+// console.log(points[4]);
+// var entries = ["Steve", 25, "Bruce", true];
+// console.log(entries[1]);
+// console.log(entries[0]);
+// console.log(entries[3]);
+// console.log(cities);
+// console.log(cities.length);
+// cities.push("Omaha");
+// console.log(cities.length);
+// console.log(cities);
+// cities.push("Ames", "Beatrice", "Springfield");
+// console.log(cities.length);
+// console.log(cities);
+// cities.pop();
+// console.log(cities.length);
+// console.log(cities);
+// cities.splice(2,2);
+// console.log(cities);
+// cities.splice(1);
+// console.log(cities);
+var company = {
+    companyName: "AIM Institute",
+    federalTaxId: 84827690,
+    sections: [
+        {
+            sectionName: "AIM Code School",
+            instructors: [
+                {
+                    firstName: "Vanessa",
+                    lastName: "Kasun"
+                },
+                {
+                    firstName: "David",
+                    lastName: "Tarvin"
+                }
+            ],
+            numOfStudents: 26,
+            sectionHead: "Erin Lasiter"
+        },
+        {
+            sectionName: "Careerlink",
+            sectionHead: "Nate Decker"
+        }
+    ]
+}
+// console.log(company);
+// console.log("AIM's federal tax id is " + company.federalTaxId);
+// console.log(company.sections[0].sectionName);
+// console.log(company.sections[0].instructors[1].firstName);
+// var count = 1;
+// console.log('Count is now ' + count);
+// count = count + 5;
+// console.log('count = count + 5 Count is now ' + count);
+// count += 3;
+// console.log('count += 3 Count is now ' + count);
+// count++;
+// console.log('count++ Count is now ' + count);
+// var x = 18;
+// var y = 9;
+// console.log(x + ' divided by ' + y + ' = ' + (x / y));
+// console.log('The remainder of ' + x + ' divided by ' + y + ' is ' + (x % y));
+// y = 12;
+// console.log(x + ' divided by ' + y + ' = ' + (x / y));
+// console.log('The remainder of ' + x + ' divided by ' + y + ' is ' + (x % y));
+// x = 24;
+// console.log(x + ' divided by ' + y + ' = ' + (x / y));
+// console.log('The remainder of ' + x + ' divided by ' + y + ' is ' + (x % y));
+var x = 9;
+// console.log('is x == 8 true or false? ' + (x == 8));
+// console.log('is x == 9 true or false? ' + (x == 9));
+// console.log('is x == "9" true or false? ' + (x == "9"));
+// console.log('is x === 9 true or false? ' + (x === 9));
+// console.log('is x === "9" true or false? ' + (x === "9"));
+// console.log('is x != 8 true or false? ' + (x != 8));
+// console.log('is x != "8" true or false? ' + (x != "8"));
+// console.log('is x != 9 true or false? ' + (x != 9));
+// console.log('is x != "9" true or false? ' + (x != "9"));
+// console.log('is x !== "9" true or false? ' + (x !== "9"));
+// console.log('is x !== 9 true or false? ' + (x !== 9));
+// console.log('is x < 10 true or false? ' + (x < 10));
+// console.log('is x < 9 true or false? ' + (x < 9));
+// console.log('is x > 8 true or false? ' + (x > 8));
+// console.log('is x > 9 true or false? ' + (x > 9));
+// console.log('is x <= 9 true or false? ' + (x <= 9));
+// console.log('is x >= 9 true or false? ' + (x >= 9));
+// var goodDeal;
+// var toyCar = 5.00;
+// goodDeal = (toyCar < 1.00) ? "Let's buy it!" : "Let's skip it.";
+// console.log(goodDeal);
+// var x = 15;
+// var y = 21;
+// console.log('x > 10 && y < 25: ' + (x > 10 && y < 25));
+// console.log('x > 20 && y < 25: ' + (x > 20 && y < 25));
+// console.log('x > 10 && y < 20: ' + (x > 10 && y < 20));
+// console.log('x > 20 && y < 20: ' + (x > 20 && y < 20));
+// console.log('#######################');
+// console.log('x > 10 || y < 25: ' + (x > 10 || y < 25));
+// console.log('x > 20 || y < 25: ' + (x > 20 || y < 25));
+// console.log('x > 10 || y < 20: ' + (x > 10 || y < 20));
+// console.log('x > 20 || y < 20: ' + (x > 20 || y < 20));
+// console.log('>>>>>>>>>>>>>>>><<<<<<<<<<<<');
+// console.log('!(x > 20): ' + !(x > 20));
+// console.log('!(x < 20): ' + !(x < 20));
+// console.log('x > 10 && !(y < 25): ' + (x > 10 && !(y < 25)));
+// console.log('!(x > 20 && y < 20): ' + !(x > 20 && y < 20));
+
+// var zombies = 28;
+// if (zombies > 30) {
+//     console.log('AAARGH! Too many zombies!!!');
+// }
+
+// zombies = 28;
+// if (zombies > 20) {
+//     console.log('AAARGH! Too many zombies!!!');
+// } else {
+//     console.log('Glad there are not too many zombies.');
+// }
+
+// if (zombies == 0) {
+//     console.log("No zombies here.");
+// } else if (zombies <= 10) {
+//     console.log("Not too many zombies here.");
+// } else if (zombies <= 20) {
+//     console.log("I'm getting scared!");
+// } else {
+//     console.log("ZOMBIES!!!");
+// }
+
+// var grade = 50;
+
+// if (grade >= 90) {
+//     console.log('You got an A');
+// } else if (grade >= 80) {
+//     console.log("You got a B");
+// } else if (grade >= 70) {
+//     console.log("You got a C");
+// } else if (grade >= 60) {
+//     console.log("You got a D");
+// } else {
+//     console.log("FAILURE!!! My son is a DONUT!");
+// }
+
+// var goldCoins = 29;
+// if (goldCoins > 5) {
+//     console.log("Cool, a bit of change!");
+// } else if (goldCoins > 10) {
+//     console.log("Great! A lot of change!");
+// } else if (goldCoins > 20) {
+//     console.log("I'm rich!!!");
+// }
+
+// var cars = ["Saab", "Subaru", "Audi", "Buick", "Chevy", "Dodge", "Ford"];
+// for (var i = 0; i < cars.length; i++) {
+//     console.log("Car " + (i + 1) + " is a " + cars[i]);
+// }
+
+// var cars = ["Saab", "Subaru", "Audi", "Buick", "Chevy", "Dodge", "Ford"];
+// for (var i = cars.length-1; i >= 0; i--) {
+//     console.log("Car " + (i + 1) + " is a " + cars[i]);
+// }
+
+// var cars = ["Saab", "Subaru", "Audi", "Buick", "Chevy", "Dodge", "Ford"];
+// for (var i = cars.length-1; i >= 0; i-=2) {
+//     console.log("Car " + (i + 1) + " is a " + cars[i]);
+// }
+
+// var cars = ["Saab", "Subaru", "Audi", "Buick", "Chevy", "Dodge", "Ford"];
+// for (var i = 0; i < cars.length; i++) {
+//     if (i % 3 == 0) {
+//         continue;
+//     } else {
+//         console.log("Car " + (i + 1) + " is a " + cars[i]);
+//     }
+// }
+
+// var cars = ["Saab", "Subaru", "Audi", "Buick", "Chevy", "Dodge", "Ford"];
+// for (var i = 0; i < cars.length; i++) {
+//     if (i > 0 && i % 3 == 0) {
+//         break;
+//     } else {
+//         console.log("Car " + (i + 1) + " is a " + cars[i]);
+//     }
+// }
+
+// var i = 1;
+// var total = 0;
+// while (total < 50) {
+//     console.log('i is ' + i);
+//     total += i;
+//     console.log('total is now ' + total);
+//     i++;
+// }
+// console.log('loop is now done');
+
+// var i = 1;
+// var total = 1;
+// do {
+//     console.log('i is ' + i);
+//     total *= i;
+//     console.log('total is now ' + total);
+//     i++;
+// } while (total < 500);
+// console.log('loop is now done');
+
+// var i = 0;
+// console.log('start of while loop');
+// while (i < 6) {
+//     console.log('i = ' + i);
+//     i++;
+// }
+// console.log('End of while loop');
+
+// var j = 0;
+// console.log('start of do-while loop');
+// do {
+//     console.log('j = ' + j);
+//     j++;
+// } while (j < 6);
+// console.log('End of do-while loop');
+
+// var i = 6;
+// console.log('start of while loop');
+// while (i < 6) {
+//     console.log('i = ' + i);
+//     i++;
+// }
+// console.log('End of while loop');
+
+// var j = 6;
+// console.log('start of do-while loop');
+// do {
+//     console.log('j = ' + j);
+//     j++;
+// } while (j < 6);
+// console.log('End of do-while loop');;
+
+// function addThreePlusFive() {
+//     var total = 0;
+//     total = 3 + 5;
+//     console.log('total is ' + total);
+// }
+
+// addThreePlusFive();
+
+// function addTwoPlusSeven() {
+//     var total = 0;
+//     total = 2 + 7;
+//     console.log('Total is ' + total);
+// }
+
+// addTwoPlusSeven();
+
+// function addTwoNumbers(num1, num2) {
+//     console.log('The total of ' + num1 + " and " + num2 + " is " + (num1 + num2));
+// }
+
+// addTwoNumbers(6, 12);
+// addTwoNumbers(17, 13);
+// addTwoNumbers(52, 48);
+// addTwoNumbers(103, 241);
+
+// var a = "Hello";
+
+// function greet() {
+//     console.log(a);
+// }
+// greet();
+// console.log(a);
+
+// function change() {
+//     a = 3;
+// }
+// console.log(a);
+// change();
+// console.log(a);
+
+// function greet() {
+//     a = "Hello";
+// }
+// greet();
+// console.log(a);
+
+// function greet() {
+//     var a = "Hello";
+// }
+// greet();
+// console.log(a);
+
+function greet() { // block 1
+    var a = "hello"; // scoped to entire function
+
+    if (a == "hello") { // block 2
+        var b = "world"; // scoped to entire function
+        console.log(a + " " + b); // hello world
+    } // end of block 2
+    console.log(a + " " + b); // hello world
+} //end of block 1
+
+// greet();
+
+function greet2() { // block 1
+    let a = "hello"; // scoped to block 1
+    if (a == "hello") { // block 2
+        let b = "world"; // scoped to block 2
+        console.log(a + " " + b); // hello world
+    } // end of block 2
+    console.log(a + " " + b); // uncaught reference error - b is not defined
+} // end of block 1
+
+// greet2();
+
+// (function showMessage() {
+//     console.log("Look at me! You didn't even call my function!");
+// })();
+
+// const shirt = "green";
+// console.log("My shirt is " + shirt);
+// shirt = "red";
+// console.log("My shirt is now " + shirt);
+
+// const shirts = ['blue', 'green', 'red'];
+// console.log(shirts);
+// shirts.push('yellow');
+// console.log(shirts);
+// shirts = ['blue', 'green', 'red', 'yellow'];
+
+// const goodGuys = ["Luke", "Han", "Lando", "Ben"];
+// goodGuys.forEach(function(guy) {
+//     console.log("I'm friends with " + guy);
+// });
+
+// document.querySelector(".title").innerHTML = "I changed the Title";
